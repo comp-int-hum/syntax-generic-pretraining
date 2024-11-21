22235d93b07a304c8e1d863ffef10a555a0f26e1
Craig <craig.messner@gmail.com>
2024-11-21 17:18:48 -0500

    initial wd query

---
+++ scripts/gather_metadata.py
@@ -0,0 +1,27 @@
+import argparse
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+#
+# This script does *nothing* except print out its arguments and touch any files
+# specified as outputs (thus fulfilling a build system's requirements for
+# success).
+#
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sparql", help="Sparql plaintext input")
+    parser.add_argument("--output", help="Output jsonl")
+    args, rest = parser.parse_known_args()
+
+    
+
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+    print("Gathering metadata {} from {}".format(args.output, args.sparql))
+    with open(args.sparql, "rt") as s_in:
+        q = s_in.read()
+        sparql.setQuery(q)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
+        print(results)
+        
+#https://www.wikidata.org/w/api.php?action=wbgetentities&format=xml&props=sitelinks&ids=Q174596&sitefilter=enwikisource        
